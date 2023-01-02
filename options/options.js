10f052632add9f5245c4e6a3febd6afd73481a56
Crowdbotics <team@crowdbotics.com>
2023-01-02 01:07:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "parforia_38149",
+  url: "https://parforia_38149.botics.co",
+  api: "https://parforia_38149.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
